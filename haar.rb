1c402b3ef88bec5d6cfdaef6f70c7c547f6b07b6
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-01-26 02:03:10 +0900

    Update haar.rb
    space

--- haar.rb
+++ haar.rb
@@ -6,39 +6,39 @@ class HaarWavelet
   # @param [Integer] scale_size スケールの大きさ, 2以上を指定
   def initialize(wave, scale_size)
     @wave = wave
-  	@difference = Array.new(scale_size)
-  	@difference[0] = @wave
-  	@weight = Array.new(scale_size)
-  	@weight[0] = Array.new(@wave.length, 0)
-  	CalcScale()
+    @difference = Array.new(scale_size)
+    @difference[0] = @wave
+    @weight = Array.new(scale_size)
+    @weight[0] = Array.new(@wave.length, 0)
+    CalcScale()
   end
 
   def CalcScale()
-  	for i in 1..@difference.length-1 do
-  		prev_wave = @difference[i-1]
-  		now_difference = Array.new(prev_wave.length >> 1)
-  		now_weight = Array.new(now_difference.length)
+    for i in 1..@difference.length-1 do
+      prev_wave = @difference[i-1]
+      now_difference = Array.new(prev_wave.length >> 1)
+      now_weight = Array.new(now_difference.length)
 
-  		for j in 0..now_difference.length-1 do
-  			weight = (prev_wave[j*2] - prev_wave[j*2+1]) * 0.5
-  			difference = prev_wave[j*2] - weight
-  			now_difference[j] = difference
-  			now_weight[j] = weight
-  		end
-  		@difference[i] = now_difference
-  		@weight[i] = now_weight
-  	end
+      for j in 0..now_difference.length-1 do
+        weight = (prev_wave[j*2] - prev_wave[j*2+1]) * 0.5
+        difference = prev_wave[j*2] - weight
+        now_difference[j] = difference
+        now_weight[j] = weight
+      end
+      @difference[i] = now_difference
+      @weight[i] = now_weight
+    end
   end
 
   # 長さを返す
   # @return [Array<Float>] スケールごとの長さ
   def Difference(scale)
-  	return @difference[scale]
+    return @difference[scale]
   end
 
   # 重みを返す
   # @return [Array<Float>] スケールごとの重み
   def Weight(scale)
-  	return @weight[scale]
+    return @weight[scale]
   end
 end
